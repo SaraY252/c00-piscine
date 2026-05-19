73d74dffee3713b835fcac19169fdc8dd3916506
SaraY252 <sarayounis252@gmail.com>
2026-05-19 22:37:06 +0300

    update c00 exercises

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,5 +1,18 @@
 #include <unistd.h>
 
-void ft_print_reverse_alphabet(void){
-    
+void ft_print_reverse_alphabet(void)
+{
+    char c = 'z';
+    while (c >= 'a')
+    {
+        write(1, &c, 1);
+        c--;
+    }
+
+}
+
+int main(void)
+{
+    ft_print_reverse_alphabet();
+    return(0);
 }
